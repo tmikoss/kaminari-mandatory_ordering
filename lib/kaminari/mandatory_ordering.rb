e4bcee2748da6343f611cb098e1096f5c8a1fc47
Toms Mikoss <toms.mikoss@gmail.com>
2017-04-05 14:43:42 +0300

    Init

---
+++ lib/kaminari/mandatory_ordering.rb
@@ -0,0 +1,7 @@
+require "kaminari/mandatory_ordering/version"
+
+module Kaminari
+  module MandatoryOrdering
+    # Your code goes here...
+  end
+end
---
+++ lib/kaminari/mandatory_ordering/version.rb
@@ -0,0 +1,5 @@
+module Kaminari
+  module MandatoryOrdering
+    VERSION = "0.1.0"
+  end
+end
