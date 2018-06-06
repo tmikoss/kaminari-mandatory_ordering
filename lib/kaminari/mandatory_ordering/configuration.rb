8c355282169fd4cba32fc2bb194eafb9ffa9783b
Toms Mikoss <toms.mikoss@gmail.com>
2018-06-06 19:03:21 +0300

    Make configuration compatible with Kaminari 1.1.1

--- lib/kaminari/mandatory_ordering/configuration.rb
+++ lib/kaminari/mandatory_ordering/configuration.rb
@@ -1,3 +1,11 @@
-Kaminari.config.instance_eval do
-  self.mandatory_ordering = true
+# Kaminari version 1.1.1 removed ActiveSupport integration
+if defined?(ActiveSupport::Configurable::Configuration) && Kaminari.config.class.ancestors.include?(ActiveSupport::Configurable::Configuration)
+  Kaminari.config.instance_eval do
+    self.mandatory_ordering = true
+  end
+else
+  Kaminari.config.class.class_eval do
+    attr_accessor :mandatory_ordering
+  end
+  Kaminari.config.instance_variable_set(:@mandatory_ordering, true)
 end
